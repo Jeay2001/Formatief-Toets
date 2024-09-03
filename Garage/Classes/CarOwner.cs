20e3311248c3f9604e6b327c6e7b98e646cb9c69
Jeay <jeay2001@gmail.com>
2024-09-03 19:07:18 +0200

    Classes + Attributes Added

---
+++ Garage/Classes/CarOwner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Classes
+{
+    public class CarOwner
+    {
+        public string Name { get; set; }
+
+        public CarOwner(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Garage/Classes/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Classes
+{
+    public abstract class Vehicle
+    {
+        public string? Description { get; set; }
+        public string LicensePlate { get; set; }
+
+        public Vehicle(string? Description, string licensePlate)
+        {
+            this.Description = Description;
+            this.LicensePlate = licensePlate;
+        }
+    }
+}
